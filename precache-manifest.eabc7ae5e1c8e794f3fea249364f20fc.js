c65bd88bfcbdb64ce967399b6437ef6eac82c7cf
jonathan theron <jonathan.theron@gmx.fr>
2020-02-06 14:15:41 +0100

    Updates

---
+++ precache-manifest.eabc7ae5e1c8e794f3fea249364f20fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d651b9820bfef214628de08c88c9c3bf",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6f31b8bafd46ee9b508b",
+    "url": "/monster-rolodex/static/css/main.7f0b9095.chunk.css"
+  },
+  {
+    "revision": "20eb7b3612173e07f0b7",
+    "url": "/monster-rolodex/static/js/2.4f0a8636.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.4f0a8636.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f31b8bafd46ee9b508b",
+    "url": "/monster-rolodex/static/js/main.5afb6f4c.chunk.js"
+  },
+  {
+    "revision": "4cf4f8a7fe2234154a3a",
+    "url": "/monster-rolodex/static/js/runtime-main.11452f00.js"
+  }
+]);
